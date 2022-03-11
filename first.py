19776ddbc265a2033da5aa7c70720f59b53f5ffb
Ujjwal Goel <8354378+ujjwal123123@users.noreply.github.com>
2022-03-11 21:45:05 +0530

    Refactor code for finding firsts
    - Fix error: `non_terminal` was named `terminal`
    - Move some from `main.py` back to `first.py`

--- first.py
+++ first.py
@@ -1,18 +1,28 @@
-from pprint import pprint
-from typing import Dict, Optional, Set
+from typing import Dict, List, Optional, Set
 
 
-def is_terminal(c: str, rules: Dict):
-    return c in rules.keys()
+def is_terminal(token: str, rules: Dict[str, List[Optional[str]]]):
+    return token not in rules.keys()
 
 
-def find_first(terminal: str, rules: Dict) -> Set[Optional[str]]:
-    ret: set[Optional[str]] = set()
-    for production in rules[terminal]:
+def find_first(
+    non_terminal: str, rules: Dict[str, List[Optional[str]]]
+) -> Set[Optional[str]]:
+    ret: Set[Optional[str]] = set()
+    for production in rules[non_terminal]:
         if production is None:
             ret.add(None)
-        elif not is_terminal(production[0], rules):
+        elif is_terminal(production[0], rules):
             ret.add(production[0])
         else:
             ret |= find_first(production[0], rules)
     return ret
+
+
+def find_firsts(rules: Dict[str, List[Optional[str]]]) -> Dict[str, Set[Optional[str]]]:
+    firsts: Dict[str, Set[Optional[str]]] = dict()
+
+    for non_terminal in rules.keys():
+        firsts[non_terminal] = find_first(non_terminal, rules)
+
+    return firsts
