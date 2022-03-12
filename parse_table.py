ebf9320a079d4850c789d14f96ecee001ba03fac
Ujjwal Goel <8354378+ujjwal123123@users.noreply.github.com>
2022-03-12 20:16:44 +0530

    Improve parse table code

--- parse_table.py
+++ parse_table.py
@@ -1,57 +1,45 @@
-from pprint import pprint
 from typing import Dict, List, Set
-
 
 def parse_table(
     rules: Dict[str, List],
     first: Dict[str, Set],
     follow: Dict[str, Set],
 ):
-    parse_table: Dict[str, Dict[str, List]] = dict()
-    non_terminals: Set[str] = set()
+    parse_table: Dict[str, Dict[str, str]] = dict()
+    non_terminals: List[str] = list(rules.keys())
+
+    def insert(non_terminal, terminal, rule):
+        if terminal in parse_table[non_terminal]:
+            print("Error: Ambiguous grammar")
+            print(f"{non_terminal} -> {parse_table[non_terminal][terminal]} and {non_terminal} -> {rule} conflict") 
+            exit(1)
+        else:
+            parse_table[non_terminal][terminal] = rule           
 
     for nt in rules.keys():
-        non_terminals.add(nt)
         parse_table[nt] = dict()
 
-    terminals: Set[str] = set()
-
-    for nt in rules.keys():
-        for r in rules[nt]:
-            if r == None:
-                terminals.add("$")
-                continue
-            for ch in r:
-                if ch not in non_terminals:
-                    terminals.add(ch)
-
-    for nt in non_terminals:
-        parse_table[nt] = dict()
-        for t in terminals:
-            parse_table[nt][t] = []
-
-    for nonterminal in rules.keys():
+    for nonterminal in non_terminals:
         for rule in rules[nonterminal]:
             if rule == None:
                 for follow_terminals in follow[nonterminal]:
-                    parse_table[nonterminal][follow_terminals].append(rule)
+                    insert(nonterminal, follow_terminals, rule)
                 continue
             for ch in rule:
                 flag: int = 0
-                if ch in terminals:
-                    parse_table[nonterminal][ch].append(rule)
+                if ch not in non_terminals:
+                    insert(nonterminal, ch, rule)
                     break
                 else:
-                    print(type(first))
                     for first_set in first[ch]:
                         if first_set == None:
                             flag = 1
                             continue
-                        parse_table[nonterminal][first_set].append(rule)
+                        insert(nonterminal, first_set, rule)
                 if flag == 0:
                     break
                 if ch == rule[len(rule) - 1]:
                     for follow_set in follow[nonterminal]:
-                        parse_table[nonterminal][follow_set].append(rule)
+                        insert(nonterminal, follow_set, rule)
 
     return parse_table
