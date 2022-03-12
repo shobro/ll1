6c068b4c16e344b590399fd59a10fe270b6aebb3
shobro <shobhitbelwal02@gmail.com>
2022-03-12 20:31:21 +0530

    added code for parsing code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import Optional, Dict, List, Set
 import first
 import follow
 import parse_table
-
+import parsing
 rules: Dict[str, List[Optional[str]]] = {
     "E": ["TX"],
     "X": ["+TX", "-TX", None],
@@ -21,3 +21,4 @@ pprint(follows)
 parse_tables = parse_table.parse_table(rules, firsts, follows)
 print("Parsing table: ")
 pprint(parse_tables)
+parsing.parsing(rules,parse_tables)
---
+++ parsing.py
@@ -0,0 +1,72 @@
+from pprint import pprint
+
+
+def classify_terminal(ch):
+    if ch == '('or ch == ')' or ch == '+' or ch == '-' or ch == '/' or ch == '*' or ch == '$':
+        return ch
+    
+    
+    for c in ch:
+        if c.isalpha():
+            return 'i'    
+    
+    return 'n'
+
+def reverse(x):
+  if x==None:
+      return None 
+  return x[::-1]
+
+def parsing(rules, parsing_tables):
+    f = open("output.txt","r")
+    lines = f.readlines()
+    input_stack = []
+    for line in lines:
+        input_stack.append(line)
+    
+    
+    non_terminals = set()
+    for nt in rules.keys():
+        non_terminals.add(nt)
+    
+    
+    terminals = set()    
+    for nt in rules.keys():
+        for r in rules[nt]:
+            if r == None:
+                #terminals.add("$")
+                continue
+            for ch in r:
+                if ch not in non_terminals:
+                    terminals.add(ch)
+    
+    
+    grammar_stack = []
+    grammar_stack.append("E")
+    input_stack.append("$\n")
+    for i in input_stack:
+        i = i[:len(i)-1]
+        t = classify_terminal(i)
+        while(True):
+            g = grammar_stack.pop()
+            if g not in rules.keys() or len(parsing_tables[g][t]) == 0:
+                print("not parsed with ",g," and ",grammar_stack," in stack")
+                return
+            for g_rules in parsing_tables[g][t]:
+                g_rules = reverse(g_rules)
+                #print(g_rules)
+                if g_rules != None:
+                    for rule in (g_rules):
+                        if rule == None:
+                            break
+                        grammar_stack.append(rule)
+            if len(grammar_stack)>0 and grammar_stack[-1] == t:
+                print(grammar_stack.pop())                
+                break
+            if len(grammar_stack) == 0:
+                break
+        if i == '$' and len(grammar_stack) == 0:
+            print("successfully parsed")
+        
+
+    
